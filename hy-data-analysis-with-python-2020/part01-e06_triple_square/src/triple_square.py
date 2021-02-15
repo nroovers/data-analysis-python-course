83aa51fb35cc69abc1f81e32b7d303a4d2c1371d
Nicolai Roovers <nroovers@gmail.com>
2021-02-15 21:15:32 +0200

    exercise1.6

--- hy-data-analysis-with-python-2020/part01-e06_triple_square/src/triple_square.py
+++ hy-data-analysis-with-python-2020/part01-e06_triple_square/src/triple_square.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
 
 
+def triple(x):
+    return x*3
+
+
+def square(x):
+    return x**2
+
+
 def main():
-    pass
+    for i in range(10):
+        t = triple(i+1)
+        s = square(i+1)
+        if t < s:
+            break
+        print(f'triple({i+1})=={t} square({i+1})=={s}')
+
 
 if __name__ == "__main__":
     main()

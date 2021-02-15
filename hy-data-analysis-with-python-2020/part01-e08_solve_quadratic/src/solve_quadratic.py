55e4843f7d7294bad740e845d5b337a699cc4875
Nicolai Roovers <nroovers@gmail.com>
2021-02-15 21:59:38 +0200

    exercise1.8

--- hy-data-analysis-with-python-2020/part01-e08_solve_quadratic/src/solve_quadratic.py
+++ hy-data-analysis-with-python-2020/part01-e08_solve_quadratic/src/solve_quadratic.py
@@ -2,12 +2,16 @@
 
 import math
 
+
 def solve_quadratic(a, b, c):
-    return (0,0)
+    sqrt = math.sqrt((b**2)-(4*a*c))
+    return ((-b+sqrt)/(2*a), (-b-sqrt)/(2*a))
 
 
 def main():
-    pass
+    print(solve_quadratic(1, -3, 2))
+    print(solve_quadratic(1, 2, 1))
+
 
 if __name__ == "__main__":
     main()

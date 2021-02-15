48c29100ed6a4776173ff2d757efc74855a0bc1a
Nicolai Roovers <nroovers@gmail.com>
2021-02-15 20:52:16 +0200

    exercise1.5

--- hy-data-analysis-with-python-2020/part01-e05_two_dice/src/two_dice.py
+++ hy-data-analysis-with-python-2020/part01-e05_two_dice/src/two_dice.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 def main():
-    pass
+    dice = [1, 2, 3, 4, 5, 6]
+    for i in dice:
+        for j in dice:
+            if i+j == 5:
+                print(f'({i}, {j})')
+
 
 if __name__ == "__main__":
     main()

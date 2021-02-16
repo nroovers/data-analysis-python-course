a014ee0c65301d9d1966d3916d852498211aef18
Nicolai Roovers <nroovers@gmail.com>
2021-02-16 21:17:35 +0200

    exercise1.10

--- hy-data-analysis-with-python-2020/part01-e10_detect_ranges/src/detect_ranges.py
+++ hy-data-analysis-with-python-2020/part01-e10_detect_ranges/src/detect_ranges.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 
 def detect_ranges(L):
-    return []
+    result=[]
+    ranges = [[]]
+    for i in sorted(L):
+        # print('nr', i)
+        if len(ranges[-1]) == 0 or ranges[-1][-1]+1 == i:
+            ranges[-1].append(i)
+            # print('appended to range', ranFge)
+        else:
+            ranges.append([i])
+            # print('range appended to result', ranges)
+    for r in ranges:
+        if len(r)==1:
+            result.append(r[0])
+        else:
+            result.append((r[0],r[-1]+1))
+    return result
+
 
 def main():
     L = [2, 5, 4, 8, 12, 6, 7, 10, 13]
     result = detect_ranges(L)
     print(L)
     print(result)
+
 
 if __name__ == "__main__":
     main()

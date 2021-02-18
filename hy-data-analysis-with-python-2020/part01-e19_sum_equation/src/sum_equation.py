f1abac51d69a188cf2d7f15eb0a86cf5308d1aa5
Nicolai Roovers <nroovers@gmail.com>
2021-02-18 20:14:24 +0200

    exercise1.19

--- hy-data-analysis-with-python-2020/part01-e19_sum_equation/src/sum_equation.py
+++ hy-data-analysis-with-python-2020/part01-e19_sum_equation/src/sum_equation.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
+from functools import reduce
+
 
 def sum_equation(L):
-    return ""
+    s1='0'
+    s2='0'
+    if len(L)>0:
+        s1 = ' + '.join(str(s) for s in L)
+        s2 = str(reduce(lambda x, y: x+y, L, 0))
+    return " = ".join([s1, s2])
+
 
 def main():
-    pass
+    print(sum_equation([1, 5, 7]))
+    print(sum_equation([]))
+
 
 if __name__ == "__main__":
     main()

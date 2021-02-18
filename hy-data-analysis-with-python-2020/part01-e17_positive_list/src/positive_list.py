5c4ee940ca6eb10691dcc9e4f5b77786e4f03b6b
Nicolai Roovers <nroovers@gmail.com>
2021-02-18 19:52:31 +0200

    exercise1.17

--- hy-data-analysis-with-python-2020/part01-e17_positive_list/src/positive_list.py
+++ hy-data-analysis-with-python-2020/part01-e17_positive_list/src/positive_list.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
 def positive_list(L):
-    return []
+    def isPos(x):
+        return x > 0
+
+    return list(filter(isPos, L))
+
 
 def main():
-    pass
+    print(positive_list([2, -2, 0, 1, -7]))
+
 
 if __name__ == "__main__":
     main()

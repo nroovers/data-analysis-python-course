16bded2a5eb97578b99c6bc2cb11a548e6420f02
Nicolai Roovers <nroovers@gmail.com>
2021-02-18 18:58:55 +0200

    exercise1.14

--- hy-data-analysis-with-python-2020/part01-e14_find_matching/src/find_matching.py
+++ hy-data-analysis-with-python-2020/part01-e14_find_matching/src/find_matching.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
 def find_matching(L, pattern):
-    return []
+    result = []
+    for i, s in enumerate(L):
+        if pattern in s:
+            result.append(i)
+    return result
+
 
 def main():
-    pass
+    print(find_matching(["sensitive", "engine", "rubbish", "comment"], "en"))
+
 
 if __name__ == "__main__":
     main()

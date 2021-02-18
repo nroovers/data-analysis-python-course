35b911f04547b6b11097994768fdee4633119ffd
Nicolai Roovers <nroovers@gmail.com>
2021-02-18 19:49:41 +0200

    exercise1.16

--- hy-data-analysis-with-python-2020/part01-e16_transform/src/transform.py
+++ hy-data-analysis-with-python-2020/part01-e16_transform/src/transform.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 
+def toInt(s):
+    return int(s)
+
+
 def transform(s1, s2):
-    return []
+    L1 = list(map(toInt, s1.split()))
+    L2 = list(map(toInt, s2.split()))
+    return [t[0]*t[1] for t in zip(L1, L2)]
+
 
 def main():
-    pass
+    print(transform("1 5 3", "2 6 -1 3"))
+
 
 if __name__ == "__main__":
     main()

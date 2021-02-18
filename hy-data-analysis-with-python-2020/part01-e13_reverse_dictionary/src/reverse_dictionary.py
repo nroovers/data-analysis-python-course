735035dbf5dadcb8914a6d079718488e487e1624
Nicolai Roovers <nroovers@gmail.com>
2021-02-18 18:55:11 +0200

    exercise1.13

--- hy-data-analysis-with-python-2020/part01-e13_reverse_dictionary/src/reverse_dictionary.py
+++ hy-data-analysis-with-python-2020/part01-e13_reverse_dictionary/src/reverse_dictionary.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
 
 def reverse_dictionary(d):
-    return {}
+    rev={}
+    for eng, finL in d.items():
+        for fin in finL:
+            if fin in rev:
+                rev[fin].append(eng)
+            else:
+                rev[fin]=[eng]
+    return rev
+
 
 def main():
-    pass
+    d = {'move': ['liikuttaa'], 'hide': ['piilottaa',
+                                         'salata'], 'six': ['kuusi'], 'fir': ['kuusi']}
+    print(reverse_dictionary(d))
+
 
 if __name__ == "__main__":
     main()

45dacedfa146c1caf4cc29bf4678471870137515
Nicolai Roovers <nroovers@gmail.com>
2021-02-18 19:17:32 +0200

    exercise1.15

--- hy-data-analysis-with-python-2020/part01-e15_two_dice_comprehension/src/two_dice_comprehension.py
+++ hy-data-analysis-with-python-2020/part01-e15_two_dice_comprehension/src/two_dice_comprehension.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 def main():
-    pass
+    L = [(i, j) for i in range(1, 7)
+         for j in range(1, 7)
+         if i+j == 5]
+
+    print(*L, sep='\n')
+
 
 if __name__ == "__main__":
     main()

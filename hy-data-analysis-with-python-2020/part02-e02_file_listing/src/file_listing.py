745916748919df933c7303048aed73e7e729cd84
Nicolai Roovers <nroovers@gmail.com>
2021-02-20 16:10:21 +0200

    exercise2.02

--- hy-data-analysis-with-python-2020/part02-e02_file_listing/src/file_listing.py
+++ hy-data-analysis-with-python-2020/part02-e02_file_listing/src/file_listing.py
@@ -4,10 +4,25 @@ import re
 
 
 def file_listing(filename="src/listing.txt"):
-    return []
+    result = []
+    with open(filename, "r") as f:
+        for line in f:
+            if len(line) > 0:
+                print(line)
+                match = re.search(r"(\d+)\s([A-Z][a-z][a-z])\s+(\d+)\s(\d\d):(\d\d)\s(\S*)\s\Z", line)
+                result.append((
+                    int(match.group(1)),
+                    match.group(2),
+                    int(match.group(3)),
+                    int(match.group(4)),
+                    int(match.group(5)),
+                    match.group(6)))
+    return result
+
 
 def main():
-    pass
+    print(file_listing("src/listing.txt"))
+
 
 if __name__ == "__main__":
     main()

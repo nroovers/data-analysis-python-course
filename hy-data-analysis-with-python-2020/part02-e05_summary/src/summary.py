ed020dfd195279e9d5db4adb54fddf543fb306eb
Nicolai Roovers <nroovers@gmail.com>
2021-02-21 15:47:31 +0200

    exercise2.05

--- hy-data-analysis-with-python-2020/part02-e05_summary/src/summary.py
+++ hy-data-analysis-with-python-2020/part02-e05_summary/src/summary.py
@@ -1,12 +1,30 @@
 #!/usr/bin/env python3
 
 import sys
+import math
+
 
 def summary(filename):
-    return (0,0,0)
+    floats = []
+    with open(filename, "r") as f:
+        for s in f:
+            try:
+                floats.append(float(s))
+            except ValueError:
+                continue
+
+    sum = math.fsum(floats)
+    avg = sum/len(floats)
+    sd = math.sqrt(
+        math.fsum([(x-avg)**2 for x in floats]) / (len(floats)-1))
+    return (sum, avg, sd)
+
 
 def main():
-    pass
+    for file in sys.argv[1:]:
+        result = summary(file)
+        print(f"File: {file} Sum: {result[0]:6f} Average: {result[1]:6f} Stddev: {result[2]:6f}")
+
 
 if __name__ == "__main__":
     main()

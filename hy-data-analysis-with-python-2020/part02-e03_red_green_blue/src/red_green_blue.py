2c39f68f54767ab32e1dd8b703d9f7766dcda239
Nicolai Roovers <nroovers@gmail.com>
2021-02-21 10:44:24 +0200

    exercise2.3

--- hy-data-analysis-with-python-2020/part02-e03_red_green_blue/src/red_green_blue.py
+++ hy-data-analysis-with-python-2020/part02-e03_red_green_blue/src/red_green_blue.py
@@ -2,12 +2,22 @@
 
 import re
 
+
 def red_green_blue(filename="src/rgb.txt"):
-    return []
+    result = []
+    with open(filename, "r") as f:
+        f.readline()
+        for line in f:
+            match = re.search(
+                r"\s*(\d+)\s+(\d+)\s+(\d+)\s[\t]+(.*)\s", line)
+            s=f"{match.group(1)}\t{match.group(2)}\t{match.group(3)}\t{match.group(4)}"
+            result.append(s)
+    return result
 
 
 def main():
-    pass
+    print(red_green_blue())
+
 
 if __name__ == "__main__":
     main()

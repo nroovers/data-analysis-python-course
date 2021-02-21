903096e865c574a3a10aea21bc06a2c49da0bcfc
Nicolai Roovers <nroovers@gmail.com>
2021-02-21 15:57:45 +0200

    exercise2.06

--- hy-data-analysis-with-python-2020/part02-e06_file_count/src/file_count.py
+++ hy-data-analysis-with-python-2020/part02-e06_file_count/src/file_count.py
@@ -2,11 +2,26 @@
 
 import sys
 
+
 def file_count(filename):
-    return (0, 0, 0)
+    lines = 0  # number of lines, words, and characters
+    words = 0
+    chars = 0
+    with open(filename, "r") as f:
+        for line in f:
+            lines += 1
+            words += len(line.split())
+            chars += (len(line))
+
+    return (lines, words, chars)
+
 
 def main():
-    pass
+    # print(file_count("src/test.txt"))
+    for file in sys.argv[1:]:
+        result = file_count(file)
+        print(f"{result[0]}\t{result[1]}\t{result[2]}\t{file}")
+
 
 if __name__ == "__main__":
     main()

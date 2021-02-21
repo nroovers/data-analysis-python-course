3d2bbb817370ce1ca68bb76034fbb0dc0f9cedaf
Nicolai Roovers <nroovers@gmail.com>
2021-02-21 11:06:15 +0200

    exercise2.04

--- hy-data-analysis-with-python-2020/part02-e04_word_frequencies/src/word_frequencies.py
+++ hy-data-analysis-with-python-2020/part02-e04_word_frequencies/src/word_frequencies.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
 
 def word_frequencies(filename):
-    return {}
+    d={}
+    with open(filename, "r") as f:
+        for line in f:
+            for s in line.split():
+                ss=s.strip("""!"#$%&'()*,-./:;?@[]_""")
+                if ss in d:
+                    d[ss]+=1
+                else:
+                    d[ss]=1
+    return d
 
 def main():
-    pass
+    print(word_frequencies('src/alice.txt'))
 
 if __name__ == "__main__":
     main()

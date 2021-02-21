692f020ad1c3da6855bf1a684a7148a71e766952
Nicolai Roovers <nroovers@gmail.com>
2021-02-21 16:28:52 +0200

    extensions2.07

--- hy-data-analysis-with-python-2020/part02-e07_file_extensions/src/file_extensions.py
+++ hy-data-analysis-with-python-2020/part02-e07_file_extensions/src/file_extensions.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python3
+import re
+
 
 def file_extensions(filename):
-    return ([], {})
+    noext = []
+    exts = {}
+    with open(filename, "r") as f:
+        for line in f:
+            match = re.search(r"\S+\.(\w+)", line)
+            if match != None:
+                ext = match.group(1)
+                if ext in exts:
+                    exts[ext].append(line.strip())
+                else:
+                    exts[ext] = [line.strip()]
+            else:
+                noext.append(line.strip())
+    return (noext, exts)
+
 
 def main():
-    pass
+    result = file_extensions("src/filenames.txt")
+    print(f'{len(result[0])} files with no extension')
+    for ext in sorted(result[1].keys()):
+        print(f'{ext} {len(result[1][ext])}')
+
 
 if __name__ == "__main__":
     main()
